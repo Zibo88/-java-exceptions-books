9116284391652bc91e509be309059c071b8721f6
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-01-31 18:04:26 +0100

    exception-book

---
+++ src/org/lessons/java/books/Book.java
@@ -0,0 +1,97 @@
+package org.lessons.java.books;
+
+public class Book {
+	
+	protected String titolo;
+	protected int numeroPagine;
+	protected String autore;
+	protected String editore;
+	
+	
+	public Book(String titolo, int numeroPagine, String autore, String editore) throws Exception {
+		super();
+		this.titolo = titolo;
+		validateTitolo(titolo);
+		this.numeroPagine = numeroPagine;
+		validePagine(numeroPagine);
+		this.autore = autore;
+		validateAutore(autore);
+		this.editore = editore;
+		validateEditore(editore);
+	}
+
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	public void validateTitolo(String titolo) throws Exception {
+		if(titolo.length()==0) {
+			throw new Exception();
+		}
+	}
+
+
+	public int getNumeroPagine() {
+		return numeroPagine;
+	}
+
+
+	public void setNumeroPagine(int numeroPagine) {
+		this.numeroPagine = numeroPagine;
+	}
+	
+	public void validePagine(int numeroPagine) throws Exception {
+		if(numeroPagine <= 0) {
+			throw new Exception();
+		}
+	}
+
+
+	public String getAutore() {
+		return autore;
+	}
+
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	public void validateAutore(String autore) throws Exception {
+		if(autore.length()<=0) {
+			throw new Exception();
+		}
+	}
+
+
+	public String getEditore() {
+		return editore;
+	}
+
+
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+	
+	public void validateEditore(String editore) throws Exception {
+		if(autore.length()<=0) {
+			throw new Exception();
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [titolo=" + titolo + ", numeroPagine=" + numeroPagine + ", autore=" + autore + ", editore="
+				+ editore + ", getTitolo()=" + getTitolo() + ", getNumeroPagine()=" + getNumeroPagine()
+				+ ", getAutore()=" + getAutore() + ", getEditore()=" + getEditore() + "]";
+	}
+	
+	
+	
+	
+}
---
+++ src/org/lessons/java/books/Main.java
@@ -0,0 +1,75 @@
+package org.lessons.java.books;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+		Scanner data = new Scanner(System.in);
+		
+		Book nuovoLibro;
+		
+		String titolo="";
+		int numeroPagine=0;
+		String autore="";
+		String editore="";
+		
+		int n = 2;
+		Book[] libreria = new Book[n];
+		
+		for(int i = 0; i <= n; i++) {
+		
+			try {
+				System.out.println("Dimmi il titolo del libro");
+				titolo = data.nextLine().toLowerCase().trim();
+				
+				try {
+					System.out.println("Dimmi il numero delle pagine del libro");
+					numeroPagine = Integer.parseInt(data.nextLine());
+				}catch(Exception e) {
+					numeroPagine = 0;
+				}
+				
+				
+				System.out.println("Dimmi chi ha scritto questo libro");
+				autore = data.nextLine();
+				System.out.println("Dimmi l'editore di questo libro");
+				editore = data.nextLine();
+				
+				nuovoLibro = new Book(titolo, numeroPagine, autore, editore);
+				nuovoLibro.setTitolo(titolo);
+				nuovoLibro.setNumeroPagine(numeroPagine);
+				nuovoLibro.setAutore(autore);
+				nuovoLibro.setEditore(editore);
+			
+				
+				nuovoLibro = libreria[i];
+				
+				
+
+			}catch(Exception e) {
+				System.out.println("errore di validazione, non è possibile istanziare un nuovo libro");
+				return;
+			}
+
+			
+		}
+		
+		data.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
